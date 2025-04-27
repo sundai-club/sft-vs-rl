0a8781065c0b2af265d325d0e2b72ab2a1f8f080
mihirathale98 <athale.m@northeastern.edu>
2025-04-27 17:07:42 -0400

    stuff

--- countdown_verifier.py
+++ countdown_verifier.py
@@ -6,13 +6,6 @@ import operator
 
 def extract_solution(solution_str):
     """Extract the equation from the solution string."""
-    # Remove everything before the first "Assistant:"
-    if "Assistant:" in solution_str:
-        solution_str = solution_str.split("Assistant:", 1)[1]
-    elif "<|im_start|>assistant" in solution_str:
-        solution_str = solution_str.split("<|im_start|>assistant", 1)[1]
-    else:
-        return None
     solution_str = solution_str.split('\n')[-1]
 
     answer_pattern = r'<answer>(.*?)</answer>'
